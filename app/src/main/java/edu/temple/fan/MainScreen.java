23f516e6a25b3fdedcd22e2a359f56e3d5177ac4
Christine Gregotski <tuj39467@temple.edu>
2021-01-30 14:42:43 -0500

    update

--- app/src/main/java/edu/temple/fan/MainScreen.java
+++ app/src/main/java/edu/temple/fan/MainScreen.java
@@ -2,9 +2,13 @@ package edu.temple.fan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -17,10 +21,36 @@ public class MainScreen extends AppCompatActivity {
 
         spinner = findViewById(R.id.selectEvent);
 
-        final String[] events = {"Choose a color", "Sports", "Concerts", "Private Event"};
+        final String[] events = {"Choose an event", "Sports", "Concerts", "Private Event"};
 
-        final BaseAdapter ColorAdapter = new EventAdapter(MainScreen.this, events);
+        final BaseAdapter EventAdapter = new EventAdapter(MainScreen.this, events);
 
+        spinner.setAdapter(EventAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ((TextView)spinner.getSelectedView()).setBackgroundColor(Color.MAGENTA);
+
+                if(events[position].equals("Red")) {
+                    //myLayout.setBackgroundColor(Color.RED);
+                }
+                else if(events[position].equals("Yellow")) {
+                    //myLayout.setBackgroundColor(Color.YELLOW);
+                }else if(events[position].equals("Green")) {
+                    //myLayout.setBackgroundColor(Color.GREEN);
+
+            }
+
+        }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+        });
 
 
     }
