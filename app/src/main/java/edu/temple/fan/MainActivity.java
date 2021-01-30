9ea7c0d42f83a83af252a4081ada5f69d5d6575f
Christine Gregotski <tuj39467@temple.edu>
2021-01-30 18:09:10 -0500

    deleted main screen class

--- app/src/main/java/edu/temple/fan/MainActivity.java
+++ app/src/main/java/edu/temple/fan/MainActivity.java
@@ -1,14 +1,46 @@
 package edu.temple.fan;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    Button chiefs;
+    Button bay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chiefs = findViewById(R.id.chiefs);
+        bay = findViewById(R.id.bay);
+
+        chiefs.setOnClickListener(new AdapterView.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent quizActivityIntent = new Intent(MainActivity.this, ChiefQuizActivity.class);
+                startActivity(quizActivityIntent);
+            }
+        });
+        bay.setOnClickListener(new AdapterView.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent quizActivityIntent = new Intent(MainActivity.this, BayQuizActivity.class);
+                startActivity(quizActivityIntent);
+            }
+        });
+
+
     }
 }
