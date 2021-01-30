859b3100b0b9ca6281bffa47e42a32ab4ea24fa4
Christine Gregotski <tuj39467@temple.edu>
2021-01-30 14:01:08 -0500

    In the process of adding a spinner that will allow the user to select which event they are attending

--- app/src/main/java/edu/temple/fan/MainScreen.java
+++ app/src/main/java/edu/temple/fan/MainScreen.java
@@ -3,12 +3,20 @@ package edu.temple.fan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainScreen extends AppCompatActivity {
+
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.selectEvent);
+
+
+
     }
 }
